a7d2dcc1bbf44817943ae9ed23c2aad4959acf24
Zülal Cengiz <zc.swe@gmail.com>
2025-09-03 17:57:34 +0300

    feat: configure image remote patterns for Clerk and Supabase

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "iviczhecfyymzqaqiukg.supabase.co",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
